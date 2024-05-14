859f63bce134e3e6965d098452a288ac4a2dd8bc
Alexandre Ornellas Cardeira <alexandreornellascardeira@yahoo.com.br>
2024-05-14 20:52:20 -0300

    Login com o Google e Facebook

---
+++ app.js
@@ -0,0 +1,93 @@
+
+
+const express = require('express')
+  , passport = require('passport')
+  , cookieParser = require('cookie-parser')
+  , session = require('express-session')
+  , bodyParser = require('body-parser')
+  , config = require('./configuration/config')
+  , facebookConfig = require('./configuration/facebookConfig')
+  , app = express();
+
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new GoogleStrategy({
+  clientID: config.api_key,
+  clientSecret: config.api_secret,
+  callbackURL: config.callback_url
+},
+  function (accessToken, refreshToken, profile, done) {
+    console.log(profile);
+    return done(null, profile);
+  }
+));
+
+
+passport.use(new FacebookStrategy({
+  clientID: facebookConfig.api_key,
+  clientSecret: facebookConfig.api_secret,
+  callbackURL: facebookConfig.callback_url
+},
+
+  function (accessToken, refreshToken, profile, done) {
+
+    console.log(profile);
+
+    return done(null, profile);
+  }
+));
+
+
+// Passport session setup.
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.render('index', { user: req.user });
+});
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login' }),
+  function (req, res) {
+    res.redirect('/');
+  }
+);
+
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+
+app.get('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+app.listen(3000, () => console.log('Server up'));
---
+++ configuration/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "api_key": "",
+    "api_secret": "",
+    "callback_url": "https://localhost:3000/auth/google/callback"
+}
