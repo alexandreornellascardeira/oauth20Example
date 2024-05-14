eb4af3254367af1b3c7619a2e6964ffc07bff043
Alexandre Ornellas Cardeira <alexandreornellascardeira@yahoo.com.br>
2024-05-14 20:58:14 -0300

    Fontes OK

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    "api_key": "",
-    "api_secret": "",
-    "callback_url": "https://localhost:3000/auth/google/callback"
-}
--- facebookConfig.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    "api_key": "",
-    "api_secret": "",
-    "callback_url": "http://localhost:3000/auth/facebook/callback"
-}
